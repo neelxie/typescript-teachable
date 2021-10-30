60c5e7512b44e545e398436e538f6e943d488471
neelxie <kidricederek@gmail.com>
2021-10-30 03:46:47 +0300

    Add status component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Greet } from './components/Greet';
+import { Status } from './components/Status';
 
 function App() {
   return (
     <div className="App">
       <Greet name="Overcomer" messageCount={10} />
+      <Status status='loading'/>
     </div>
   );
 }
