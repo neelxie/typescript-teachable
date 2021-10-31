4f312be93eed813c2ecf2c9a293454a951a8ad6b
neelxie <kidricederek@gmail.com>
2021-10-31 05:04:47 +0300

    Add button in app.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Button } from './components/Button';
 import { Greet } from './components/Greet';
 import { Status } from './components/Status';
 
@@ -7,6 +8,9 @@ function App() {
     <div className="App">
       <Greet name="Overcomer" messageCount={10} />
       <Status status='loading'/>
+      <Button handleClick={(event, id) => {
+        console.log(event, id);
+      }}/>
     </div>
   );
 }
